7fe841c3b58a690ee94abcba54465ebcdf6873e4
Abel N. Nieva <931497+abelnnieva@users.noreply.github.com>
2023-12-15 20:37:45 +0000

    update: crud on a rest api

--- src/app/common/services/courses.service.ts
+++ src/app/common/services/courses.service.ts
@@ -1,33 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Course } from '../models/course';
+
+const BASE_URL = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
-  courses: Course[] = [
-    {
-      id: '1',
-      title: 'Angular 13 Fundamentals',
-      description: 'Learn the fundamentals of Angular 13',
-      percentComplete: 12,
-      favorite: true,
-    },
-    {
-      id: '2',
-      title: 'JavaScript The Hardest Parts Ever!',
-      description: 'Learn the JavaScript like a pro! with Will',
-      percentComplete: 98,
-      favorite: true,
-    },
-    {
-      id: '3',
-      title: 'Rapid Application Development Patterns',
-      description: 'This must be done by 3:30 or else!',
-      percentComplete: 75,
-      favorite: true,
-    },
-  ];
+  model = 'courses';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  all() {
+    return this.http.get(this.getUrl());
+  }
+
+  find(id) {
+    return this.http.get(this.getUrlWithId(id));
+  }
+
+  create(course) {
+    return this.http.post(this.getUrl(), course);
+  }
+
+  update(course) {
+    return this.http.put(this.getUrlWithId(course.id), course);
+  }
+
+  delete(id) {
+    return this.http.delete(this.getUrlWithId(id));
+  }
+
+  private getUrl() {
+    return `${BASE_URL}/${this.model}`;
+  }
+
+  private getUrlWithId(id) {
+    return `${this.getUrl}/${id}`;
+  }
 }
